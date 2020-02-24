04168003e8b10968d0e6902f37eacbee856933fd
AndreaGold99 <andreainazuma@gmail.com>
2020-02-24 12:18:20 +0100

    animando aguila
    Estoy en ello

---
+++ Assets/Scripts/MovimientoPlataformep/aguila.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class aguila : MonoBehaviour
+{
+    //Variables
+    Animator anim;
+    bool girar = true;
+    SpriteRenderer aguilaRender;
+
+    //Funciones -----------------------------------------
+
+    private void Voltear()
+    {
+        voltearCaballero = !voltearCaballero;
+        caballeroRender.flipX = !caballeroRender.flipX;
+    }
+
+    //-----------------------------------------------------
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        aguilaRender = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
